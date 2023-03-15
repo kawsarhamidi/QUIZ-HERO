061acb4b958371437010d8b95f8171989a428d78
kawsarhamidi <kawsarhamidi77@gmail.com>
2023-03-16 01:00:16 +0600

    add to exta page

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ document.querySelector("#submit").addEventListener("click", () => {
     displayResult.innerHTML = `<div
     class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]"
   >
-    <h3 class="text-xl ${grade.color}">${grade.status}</h3>
+    <h3 class="text-xl" ${grade.color}>${grade.status}</h3>
     <h1 class="text-3xl font-bold my-2">
       ${totalMark}<span class="text-slate-800">/60</span>
     </h1>
@@ -155,9 +155,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   </div>
   
   <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
-  ${
-    storage
-      ? `<div class="mt-5">
+  ${storage? `<div class="mt-5">
       <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
     <div
     class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
@@ -165,8 +163,7 @@ document.querySelector("#submit").addEventListener("click", () => {
     <div>Grade</div>
     <div>Time</div>
     </div>
-    ${storage
-      ?.reverse()
+    ${storage?.reverse()
       ?.map(
         (item) => `<div
       class="flex justify-between items-center border rounded p-2 my-2 shadow-sm">
